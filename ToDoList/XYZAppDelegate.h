1058a495cd57a1fe4357caf900ae2b877528ff3d
Luke Mueller <luke.jordan.mueller@gmail.com>
2014-08-03 17:09:07 -0500

    Adds empty project with hello world storyboard

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Luke Mueller on 8/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
